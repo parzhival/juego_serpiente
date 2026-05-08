c0bcf941468bf44a73e4bc3c1c07e9a53b56b18d
parzhival <jean08070225@gmail.com>
2026-05-07 21:20:10 -0500

    feat: draw board is added

--- serpiente.js
+++ serpiente.js
@@ -2,7 +2,8 @@
     // 1. Capturamos el canvas y su contexto de dibujo
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
-
+    
+    const TAMANIO_CELDA = 25;
 
     
 
@@ -19,7 +20,32 @@
 
     function dibujarTodo() {
       limpiarCanvas();
+      dibujarTablero2();
     }
 
+    function dibujarTablero(){
+      ctx.strokeStyle = "rgba(17, 186, 70, 0.73)"
+      ctx.beginPath();// empieza a dibujar en el canva 
+      ctx.moveTo(0,0); //donde empieza a dibujar
+      ctx.lineTo(100,100); //hasta donde se dibuja en el canva     
+      ctx.stroke(); //pinta contorno de la figura 
+    }
+
+    function dibujarTablero2(){
+      for (let i = 0; i<canvas.width; i+=TAMANIO_CELDA){
+        ctx.strokeStyle = "rgba(17, 186, 70, 0.73)"
+        ctx.beginPath();
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,canvas.height);
+        ctx.stroke();
+      }
+      for( let i = 0 ; i<canvas.height; i +=TAMANIO_CELDA){
+        ctx.strokeStyle = "rgba(17, 186, 70, 0.73)"
+        ctx.beginPath();
+        ctx.moveTo(0,i);
+        ctx.lineTo(canvas.width,i);
+        ctx.stroke();
+      }
+    }
 
 
